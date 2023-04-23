12cf8f211bfe00dd42596574b537f53f8efa82e0
jiaxuhui <j1991225@live.com>
2023-04-23 16:24:52 +0800

    1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var request = require('request')
 var fs = require('fs')
 const cheerio = require("cheerio")
-const maxCid = 2189//2245
+const endCid = 2191//2245
 const startCid = 2189 //2189
 let url = 'https://www.cool18.com/bbs4/index.php?app=book&act=bookview&cid='
 const path = './docs/sjd'
@@ -11,7 +11,7 @@ if (!fs.existsSync(path)) {
 getData(startCid)
 
 function getData(cid) {
-  if (cid > maxCid) {
+  if (cid > endCid) {
     return
   }
   const r_url = url + cid
@@ -23,13 +23,21 @@ function getData(cid) {
     if (!err) {
       const $ = cheerio.load(body);
       let title = $('meta[name="Description"]').attr('content')
-      let content = $('pre').html()//.replace(/<p>/g, '\n')
+      let content = title + '<br>' + $('pre').html()//.replace(/<p>/g, '\n')
       content = content.replace(/<font color="#E6E6DD">cool18.com<\/font>/g, '')
       // content = content.replace(/<\/p>/g, '\n')
       // content = content.replace(/<br>/g, '\n')
-
+      let lastPage = ''
+      let nextPage = ''
+      if (cid > startCid) {
+        lastPage = '<a href="./' + (cid - 1) + '.html">上一页</a>'
+      }
+      if (cid < endCid) {
+        nextPage = '<a href="./' + (cid + 1) + '.html">下一页</a>'
+      }
+      content += `<div>${lastPage}${nextPage}</div>`
       // console.log(content)
-      fs.writeFile(path + '/' + cid + '.html', title + '<br>' + content, 'utf-8', (err, data) => {
+      fs.writeFile(path + '/' + cid + '.html', content, 'utf-8', (err, data) => {
         console.log('----writeFile--err------', err)
         // console.log('------data------', data)
         getData(++cid)
